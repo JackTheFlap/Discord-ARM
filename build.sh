3da259787b9ba19e3d997feb32c8d951e955f3ae
Jack <JackTheFlap@users.noreply.github.com>
2021-01-05 07:49:14 +0000

    Added build.sh electron-packager install prompt
    If ! node_modules folder then prompt user to install electron-packager

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+nodeInitDir=./node_modules
+if [ ! -d "$nodeInitDir" ]; then
+    #Install electron-packager
+    echo "Error: node_modules not found: Electron-packager required!"
+    read -p "Install electron-packager?" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        npm install electron-packager -g
+    fi
+fi
+
 echo "Starting Discord-ARM Build"
 electron-packager . --overwrite --platform=darwin --arch=arm64 --icon=assets/icons/icon.icns --prune=true --out=release-builds
